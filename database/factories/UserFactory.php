a672a6623fa47a078f82ba2e97b95ea29e72c14c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-02-15 11:16:26 +0100

    setting name for the app

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => 'Abdelhaq',
             'email' => 'abdelhakazour3@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('admin123'),
             'picture'=> 'admin.png',
             'remember_token' => Str::random(10),
 
